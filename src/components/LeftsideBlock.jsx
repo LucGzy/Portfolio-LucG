92fe900789af13bd675f3c0055106f4d9e1130b2
Luc Gouzy <lg-1@live.fr>
2023-12-01 00:42:21 +0100

    fixed some css

--- src/components/LeftsideBlock.jsx
+++ src/components/LeftsideBlock.jsx
@@ -16,13 +16,15 @@ export default function LeftsideBlock() {
         }}
         className=" bg-white shadow-sm ml-0 m-5"
       ></div>
-      <p className="h-fit w-[70%] font-body text-slate-50">
+      <p className=" text-2xl leading-8  h-fit w-[70%] font-body text-slate-50">
         Passionné par les technologies de l'information et de la communication
-        ainsi que la musique depuis longtemps, j'ai d'abord obtenu un diplôme
-        d'ingénieur du son. Par la suite, j'ai voulu approfondir mes bases
-        personnelles dans le domaine du développement. J'ai donc suivi une
-        formation diplômante sur OpenClassrooms dans le développement web
-        front-end et me voici maintenant ici, prêt à entreprendre vos projets.
+        ainsi que la musique, j'ai d'abord obtenu un diplôme d'ingénieur du son.
+        <br />
+        Par la suite, j'ai voulu approfondir mes bases personnelles dans le
+        domaine du développement.
+        <br /> J'ai donc suivi une formation diplômante sur OpenClassrooms dans
+        le développement web front-end et me voici maintenant ici, prêt à
+        entreprendre vos projets.
       </p>
     </div>
   );
--- src/components/RightsideBlock.jsx
+++ src/components/RightsideBlock.jsx
@@ -6,25 +6,31 @@ export default function RighsideBlock() {
   return (
     <div
       id="right-block"
-      className=" w-[50%] pt-4 h-full flex flex-col pl-24 items-center"
+      className=" w-[50%] pt-48 h-full flex flex-col  items-center"
     >
-      <img className=" w-[50%] rounded-xl  mb-4" src={profilepic} />
-      <div className=" items-start">
-        <div className="flex flex-row items-center">
-          <FaCarSide style={{ marginRight: "15px" }} />
-          <p className="font-body font-medium text-white">
+      <img className=" w-[40%] rounded-xl  mb-4" src={profilepic} />
+      <div className=" w-full flex flex-col items-start pl-[30%]">
+        <div className=" mb-2 flex flex-row items-center">
+          <FaCarSide style={{ marginRight: "15px" }} className="text-xl" />
+          <p className="text-xl font-body font-medium text-white">
             Transport : Voiture et Permis B
           </p>
         </div>
-        <div className="flex flex-row items-center">
-          <FaSearchLocation style={{ marginRight: "15px" }} />
-          <p className="font-body  text-white ">
+        <div className="mb-2 flex flex-row items-center">
+          <FaSearchLocation
+            style={{ marginRight: "15px" }}
+            className="text-xl"
+          />
+          <p className="text-xl font-body  text-white ">
             Localisation : Departement de la Vienne, France
           </p>
         </div>
         <div className="flex flex-row">
-          <FaFolderPlus style={{ marginRight: "15px" }} className="mt-1" />
-          <p className="font-body  text-white">
+          <FaFolderPlus
+            style={{ marginRight: "15px" }}
+            className="text-xl mt-1"
+          />
+          <p className=" text-xl font-body  text-white">
             Autres compétences :
             <br />
             <span style={{ marginLeft: "20px" }}>-Ingénieur du son</span>
