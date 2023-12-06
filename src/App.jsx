fda643692b49c8bfed1b1e42323f23ed03e717b5
Luc Gouzy <lg-1@live.fr>
2023-12-06 01:49:17 +0100

    added homepage + used hashrouer instead of browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import ProjectPage from "./pages/ProjectPage";
 
