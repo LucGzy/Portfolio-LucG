c1a6017d8e14607e5d42925ad7c20e5b0144952a
Luc Gouzy <lg-1@live.fr>
2024-03-11 22:36:50 +0100

    removed useless import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import LeftsideBlock from "../components/LeftsideBlock";
 import ProjectGrid from "../components/Projects";
 import RighsideBlock from "../components/RightsideBlock";
 import Footer from "../components/Footer";
-import Keyboard3D from "../components/Keyboard3D";
+
 export default function Home() {
   return (
     <div
